f152e8f8377804017f69417d66fa6edd6524b35b
linagodbole98 <linagodbole98@gmail.com>
2022-11-15 20:52:31 +0530

    insta

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-const express= require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer")
+const app = express();
+const dotenv= require("dotenv")
+const route = require("./route/routes")
 
 
-express.json()
+dotenv.config();
 
-const app = express()
+//------------------- Global or Application level Middleware-------------------//
+app.use(bodyParser.json());
+app.use(multer().any()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//  ------------------- Connection Establishment Between Application and Database -------------------//
+mongoose.connect("mongodb+srv:linagodbole99:dAix1EtU6C6yxJDR@cluster0.oip3eje.mongodb.net/Instagram",
+    { useNewUrlParser: true,})
+  .then(() => console.log("MongoDb is connected!"))
+  .catch((err) => console.log(err));
+  
+
+ //  --------middleware---------- 
+app.use(express.json());
+
+
+app.use("/", route);
 
 
 
+app.use("*", (req, res) => {
+  return res
+    .status(400)
+    .send({ status: false, message: "please enter valid url endpoint" });
+});
+
+//------------------- Server Configuration -------------------//
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
 
 
-
-
-
-
-
-
-
-app.use("/", route)
 
